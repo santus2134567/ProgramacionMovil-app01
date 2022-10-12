d9e888a7fef068d448bb00e8fe0ac73fefe172ab
ProgramacionMovil app01 <201913176e@utea.edu.pe>
2022-10-11 22:29:42 -0500

    version 1.2

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,10 @@
 import 'package:flutter/material.dart';
 
-void main(List<String> args) {
+void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -15,9 +13,101 @@ class MyApp extends StatelessWidget {
         body: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            CircleAvatar(
+            const CircleAvatar(
               radius: 60.0,
               backgroundColor: Colors.white,
+              backgroundImage: AssetImage("assets/images/persona1.jpeg"),
+            ),
+            const SizedBox(
+              height: 30.0,
+            ),
+            const Text(
+              "Fiorela de Fátima Guadalupe",
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 35.0,
+                fontWeight: FontWeight.w800,
+                fontFamily: "Dancing",
+                fontStyle: FontStyle.italic,
+              ),
+            ),
+            const Text(
+              "FLUTTER DEVELOPER",
+              style: TextStyle(
+                color: Colors.white38,
+                fontSize: 18.0,
+                fontWeight: FontWeight.w300,
+                letterSpacing: 2.0,
+              ),
+            ),
+            const Divider(
+              thickness: 0.70,
+              color: Colors.white38,
+              indent: 120.0,
+              endIndent: 120.0,
+            ),
+            const SizedBox(
+              height: 10.0,
+            ),
+            const Card(
+              color: Colors.white,
+              margin: EdgeInsets.symmetric(horizontal: 15.0, vertical: 8.0),
+              elevation: 10.0,
+              child: ListTile(
+                title: Text("+51 983707845"),
+                subtitle: Text("Teléfono"),
+                leading: Icon(
+                  Icons.phone,
+                  color: Colors.indigo,
+                ),
+                trailing: Icon(
+                  Icons.check_circle_outline,
+                  color: Colors.indigo,
+                ),
+              ),
+            ),
+            const Card(
+              color: Colors.white,
+              margin: EdgeInsets.symmetric(horizontal: 15.0, vertical: 8.0),
+              elevation: 10.0,
+              child: ListTile(
+                title: Text("eduardochavezv@gmail.com"),
+                subtitle: Text("Correo electrónico"),
+                leading: Icon(
+                  Icons.mail,
+                  color: Colors.indigo,
+                ),
+                trailing: Icon(
+                  Icons.check_circle_outline,
+                  color: Colors.indigo,
+                ),
+              ),
+            ),
+            const SizedBox(
+              height: 30.0,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Image.asset(
+                  "assets/images/facebook.png",
+                  height: 60.0,
+                ),
+                const SizedBox(
+                  width: 30.0,
+                ),
+                Image.asset(
+                  "assets/images/instagram.png",
+                  height: 60.0,
+                ),
+                const SizedBox(
+                  width: 30.0,
+                ),
+                Image.asset(
+                  "assets/images/twitter.png",
+                  height: 60.0,
+                ),
+              ],
             ),
           ],
         ),
